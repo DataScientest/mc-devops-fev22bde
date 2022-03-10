834cc6a8457e850a3fb624c8e1ff75d196103385
pauldechorgnat <pdechorgnat@yahoo.fr>
2022-03-10 11:29:18 +0100

    ajout fonctionnalité

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import requests
+import os
 
 api = FastAPI()
 
@@ -12,7 +14,18 @@ def get_index():
 
 @api.get('/temperature')
 def get_temperature(lat: float, lon: float):
+    response = requests.get(
+        url="https://api.openweathermap.org/data/2.5/weather",
+        params={
+            "lat": lat,
+            "lon": lon,
+            "appid": os.environ.get("API_KEY")
+        }
+    )
+
+    data = response.json()
     return {
         "lat": lat,
-        "lon": lon
+        "lon": lon,
+        "temp": data["main"]["temp"]
     }
